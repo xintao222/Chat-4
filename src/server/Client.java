b7138bae4b4937ebdf55a872df097c378c595199
jakkra <ijakkra@gmail.com>
2014-03-20 18:49:00 +0100

    Removed unused class

--- src/server/Client.java
+++ src/server/Client.java
@@ -57,12 +57,7 @@ public class Client implements Runnable {
 				}
 
 			}
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+			
 		}
 		try {
 			is.close();
